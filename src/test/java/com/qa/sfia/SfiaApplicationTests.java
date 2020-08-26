cb67ec4d3d71a319a5f2dc3dae8073482aedcffb
KeenanB <k.bratley626@gmail.com>
2020-08-26 17:35:09 +0100

    Initial commit

---
+++ src/test/java/com/qa/sfia/SfiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.sfia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

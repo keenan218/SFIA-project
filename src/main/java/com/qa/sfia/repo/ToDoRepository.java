8000a75496852082800d6475b44b841c0f44153d
KeenanB <k.bratley626@gmail.com>
2020-08-26 18:57:55 +0100

    ToDo Repo file

---
+++ src/main/java/com/qa/sfia/repo/ToDoRepository.java
@@ -0,0 +1,10 @@
+package com.qa.sfia.repo;
+
+
+import com.qa.sfia.domain.ToDo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToDoRepository extends JpaRepository<ToDo, Long> {
+}

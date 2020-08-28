754130082d7a506dd39711990f5dcb0a649150d3
KeenanB <k.bratley626@gmail.com>
2020-08-28 14:15:45 +0100

    Added @Repo to file

--- src/main/java/com/qa/sfia/repo/WishListRepository.java
+++ src/main/java/com/qa/sfia/repo/WishListRepository.java
@@ -2,9 +2,10 @@ package com.qa.sfia.repo;
 
 import com.qa.sfia.domain.WishList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface WishListRepository extends JpaRepository<WishList, Long> {
 
 }

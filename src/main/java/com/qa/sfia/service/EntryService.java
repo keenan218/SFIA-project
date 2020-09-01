a4d2fc58581eb66237aa77b6fb919557cd95854d
KeenanB <k.bratley626@gmail.com>
2020-09-01 15:50:02 +0100

    updated service files to be compatible with DTOs

--- src/main/java/com/qa/sfia/service/EntryService.java
+++ src/main/java/com/qa/sfia/service/EntryService.java
@@ -1,39 +1,49 @@
 package com.qa.sfia.service;
 
 import com.qa.sfia.domain.Entry;
+import com.qa.sfia.dto.EntryDTO;
 import com.qa.sfia.exceptions.EntryNotFoundException;
 import com.qa.sfia.repo.EntryRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EntryService {
 
     private final EntryRepository repo;
 
+    private final ModelMapper mapper;
+
     @Autowired
-    public EntryService(EntryRepository repo) {
+    public EntryService(EntryRepository repo, ModelMapper mapper) {
         this.repo = repo;
+        this.mapper = mapper;
+    }
+    private EntryDTO mapToDTO(Entry entry){
+        return this.mapper.map(entry, EntryDTO.class);
     }
 
-    public List<Entry> readAllEntries() {
-        return this.repo.findAll();
+    public List<EntryDTO> readAllEntries() {
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public Entry createEntry(Entry entry){
-        return this.repo.save(entry);
+    public EntryDTO createEntry(Entry entry){
+        return this.mapToDTO(this.repo.save(entry));
     }
 
-    public Entry findEntryById(Long id){
-        return this.repo.findById(id).orElseThrow(EntryNotFoundException::new);
+    public EntryDTO findEntryById(Long id){
+        return this.mapToDTO(this.repo.findById(id)
+                .orElseThrow(EntryNotFoundException::new));
     }
 
-    public Entry updateEntry(Long id, Entry entry){
-        Entry update = findEntryById(id);
+    public EntryDTO updateEntry(Long id, Entry entry){
+        Entry update = this.repo.findById(id).orElseThrow(EntryNotFoundException::new);
         update.setDescription(entry.getDescription());
-        return this.repo.save(update);
+        return this.mapToDTO(this.repo.save(entry));
     }
 
     public Boolean deleteEntryById(Long id){

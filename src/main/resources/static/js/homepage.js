cb942e830e2118d8a9d7ae361f638e9f6a0cc096
KeenanB <k.bratley626@gmail.com>
2020-09-01 20:42:31 +0100

    homepage javascript file

---
+++ src/main/resources/static/js/homepage.js
@@ -0,0 +1,42 @@
+function displayWishlists(){
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = () => {
+        // Example handle logic
+        if (req.status === 200 && req.readyState === 4) { // status 200 = 'ok'
+            if (req.getResponseHeader("Content-Type") === "application/json") {
+                console.log("oh look its some JSON: " + req.responseText);
+                // adding an element to the body example
+                // let elem = document.createElement('div');
+                // elem.textContent = "hello world";
+                // document.body.appendChild(elem);
+
+                let stuff = JSON.parse(req.response);
+                stuff.forEach(el => {
+                    // console.log(el); // prints whole element
+                    // console.log(el.name); // allows access to specific value
+
+                    // adding title to the body of the page
+                    let elem = document.createElement('div');
+                    let header = document.createElement('h1');
+                    header.textContent = "Wishlist name: " + el.name;
+                    elem.appendChild(header);
+                    el.entries.forEach(note => {
+                        console.log(note) // print all notes for each wishlist
+                        let description = document.createElement('p');
+                        description.textContent = "Description: " + entry.description;
+                        elem.appendChild(description);
+                    })
+                    document.body.appendChild(elem);
+                });
+            } else {
+                console.log(
+                    "Looks like its not JSON but lets see what it is... " + req.responseText
+                );
+            }
+        } else {
+            console.log("Oh no... handle error");
+        }
+    };
+    req.open("GET", "http://localhost:8080/getAllNoteBooks");
+    req.send();
+}

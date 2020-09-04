98d7e50c84dbcb1391a87607581128b6600bb15c
KeenanB <k.bratley626@gmail.com>
2020-09-04 19:55:57 +0100

    small updates

--- src/main/resources/static/js/createEntry.js
+++ src/main/resources/static/js/createEntry.js
@@ -9,7 +9,7 @@ function submitEntry(){
     const req = new XMLHttpRequest();
     req.open("POST", "http://localhost:8080/createEntry");
     req.onload = () => {
-        if (req.status === 200 && req.readyState == 4) {
+        if (req.status === 200 && req.readyState === 4) {
             console.log("Server Responded with: " + req.responseText);
         } else {
             console.log("Oops...");
--- src/main/resources/static/js/createWishList.js
+++ src/main/resources/static/js/createWishList.js
@@ -9,7 +9,7 @@ function createWishList(){
     const req = new XMLHttpRequest();
     req.open("POST", "http://localhost:8080/wl/createWishList");
     req.onload = () => {
-        if (req.status === 200 && req.readyState == 4) {
+        if (req.status === 200 && req.readyState === 4) {
             console.log("Server Responded with: " + req.responseText);
         } else {
             console.log("Oops...");
